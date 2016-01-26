210fb086e608646a2a6992dcb159e63942fbead8
Colin Hummel <chummel4@mail.valenciacollege.edu>
2016-01-26 14:48:08 -0500

    retry

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,21 @@
 # Colin Hummel
 
 # I set the lang variable to open, had the message print, and after the users input is gathered the lang is assigned a property which then displays the proper greeting.
->>>lang = ''
->>>while lang != 'your lang':
->>> print('Hello World! Please select your prefered language and I will greet you in it!')
+lang = ''
+while lang != 'your lang':
+	print('Hello World! Please select your prefered language and I will greet you in it!')
 
->>> print('1. German')
->>> print('2. French')
->>> print('3. Irish')
+print('1. German')
+print('2. French')
+print('3. Irish')
 
->>> lang = input()
->>> print('%' %lang)
+lang = input()
+print('%' %lang)
 
->>>if lang != 'German':
+if lang != 'German':
 	print('Hallo')
->>>if lang != 'French':
+if lang != 'French':
 	print('Bonjour')
->>>if lang != 'Irish':
+if lang != 'Irish':
 	print('Dia daoibh')
 
